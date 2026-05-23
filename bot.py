782ec36dc19478e0bb30d2e274eb5936ac837541
TDBotDev <tdbotdev1@gmail.com>
2026-05-23 22:11:28 +0530

    Add deployment notes and credit information
    Added comments for credit and deployment notes.

--- bot.py
+++ bot.py
@@ -168,3 +168,11 @@ if __name__ == "__main__":
 
     loop = asyncio.get_event_loop()
     loop.run_until_complete(main())
+#==================°°°°°°°°°°===========
+___remov this ofter deploy ______
+
+#========================================================================
+# Don't Remove Credit Tg - @TDBotDev
+# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@TDBotDev
+# Ask Doubt on telegram https://t.me/TDBotDev
+#========================================================================
