0545d611b252b44538daad35391fb28bbca7fb6b
TDBotDev <tdbotdev1@gmail.com>
2026-05-23 22:18:12 +0530

    Clean up bot.py by removing deployment comment
    Removed unnecessary comment and cleanup code.

--- bot.py
+++ bot.py
@@ -168,8 +168,6 @@ if __name__ == "__main__":
 
     loop = asyncio.get_event_loop()
     loop.run_until_complete(main())
-#==================°°°°°°°°°°===========
-___remov this ofter deploy ______
 
 #========================================================================
 # Don't Remove Credit Tg - @TDBotDev
