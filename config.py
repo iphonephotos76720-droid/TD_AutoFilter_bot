fdecc3419f98fbef0b0663b451b4e5317be711d1
iphonephotos76720-droid <iphonephotos76720@gmail.com>
2026-05-24 01:11:32 +0530

    Update config.py

--- config.py
+++ config.py
@@ -29,8 +29,8 @@ DATABASE_NAME = os.environ.get("DATABASE_NAME", "autofilebot")
 COLLECTION_NAME = "files"
 
 # Optional settings
-OWNER_ID = int(os.environ.get("OWNER_ID", 1573111356))
-ADMINS = [int(x) for x in os.environ.get("ADMINS", "1573111356").split(",") if x]  # multiple admins allowed ("8475661555","8475661555")
+OWNER_ID = int(os.environ.get("OWNER_ID", 1208374738))
+ADMINS = [int(x) for x in os.environ.get("ADMINS", "1208374738").split(",") if x]  # multiple admins allowed ("8475661555","8475661555")
 
 # Force Subscribe Settings
 # Updated to multiple channels as per user request
