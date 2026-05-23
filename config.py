f58d75c95fe0d88d88317b9b9d29763c255c37d7
TDBotDev <tdbotdev1@gmail.com>
2026-05-23 22:52:36 +0530

    Update config.py

--- config.py
+++ config.py
@@ -10,12 +10,12 @@ from dotenv import load_dotenv
 # config.py
 load_dotenv()
 
-API_ID = int(os.environ.get("API_ID", 32182319))
-API_HASH = os.environ.get("API_HASH", "773fe7e0f7247b43a79e3c2235385853")
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "8988270006:AAF4lbJgDiHN3y0cBZj3f4BFyRUbaJOR6zs")
+API_ID = int(os.environ.get("API_ID", )
+API_HASH = os.environ.get("API_HASH", "")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
 
 # Search Settings
-DB_CHANNEL_ID = int(os.environ.get("DB_CHANNEL_ID", -1003947486592))
+DB_CHANNEL_ID = int(os.environ.get("DB_CHANNEL_ID", -100))
 START_TEXT = os.environ.get("START_TEXT", "🚀 Welcome to Movie Serch \n\n🎬 Movies • Series • Anime • Episodes — All in One Place\n⚡ Lightning Fast Search with Smart Auto Filters\n🧠 Auto Detect Language • Quality • Seasons\n\n📂 Continue Watching • 🔥 Trending • ▶️ One Click Play\n\n🔍 Just send any name (movie / series / file) to start exploring")
 MAX_RESULTS = int(os.environ.get("MAX_RESULTS", 10))
 
@@ -24,7 +24,7 @@ HELP_TEXT = os.environ.get("HELP_TEXT", "📖 **Help Menu**\n\n1. Send any movie
 ABOUT_TEXT = os.environ.get("ABOUT_TEXT", "❄️ **About This Bot**\n\nThis is a high-speed file storage and search bot for CineVerse users. It indexes thousands of files and provides them with minimal delay.\n\nDeveloper: [ @TDBotDev ]")
 
 # Database Settings
-MONGO_URI = os.environ.get("MONGO_URI", "mongodb+srv://tdanimehub_db_user:/?retryWrites=true&w=majority&appName=Helper")
+MONGO_URI = os.environ.get("MONGO_URI", "")
 DATABASE_NAME = os.environ.get("DATABASE_NAME", "autofilebot")
 COLLECTION_NAME = "files"
 
@@ -34,12 +34,12 @@ ADMINS = [int(x) for x in os.environ.get("ADMINS", "1573111356").split(",") if x
 
 # Force Subscribe Settings
 # Updated to multiple channels as per user request
-FORCE_SUB_CHANNELS = [int(x) for x in os.environ.get("FORCE_SUB_CHANNELS", "-1003314977013,-1003923865001,-1002603005939").split(",") if x] # multiple force sub allowed ("-1003511440278","-1003511440278")
+FORCE_SUB_CHANNELS = [int(x) for x in os.environ.get("FORCE_SUB_CHANNELS", "-100").split(",") if x] # multiple force sub allowed ("-1003511440278",-1003511440278")
 ADMIN_IDS = ADMINS + [OWNER_ID]
 FORCE_SUB_TEXT = os.environ.get("FORCE_SUB_TEXT", "📥 **Please join our channels to use this bot!**\n\nDue to high server load, only subscribers can search files.")
 
 # Updates Link
-UPDATES = os.environ.get("UPDATES", "https://t.me/+2Ty1_CDfHNthN2M1")
+UPDATES = os.environ.get("UPDATES", "https://t.me/Team_TD_Link")
 
 # Auto-Delete Settings
 AUTO_DELETE_TIME = os.environ.get("AUTO_DELETE_TIME", "30s") # Format: 30s, 1m, 1h, 1d or '0' to disable
